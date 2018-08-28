85be399443097d293275e77e6f5f09151b7d0d8b
dandua98 <danish@dandua.com>
2018-08-27 23:48:22 -0600

    add auth and sample server

---
+++ authentication.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/thoas/go-funk"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/contrib/sessions"
+)
+
+func AuthenticationRequired(auths ...string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		user := session.Get("user")
+		if user == nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "user needs to be signed in to access this service"})
+			c.Abort()
+			return
+		}
+		if len(auths) != 0 {
+			authType := session.Get("authType")
+			if authType == nil || !funk.ContainsString(auths, authType.(string)) {
+				c.JSON(http.StatusForbidden, gin.H{"error": "invalid request, restricted endpoint"})
+				c.Abort()
+				return
+			}
+		}
+		c.Next()
+	}
+}
---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/thoas/go-funk"
+	"fmt"
+)
+
+// Note: This is just an example for a tutorial
+
+var(
+	VALID_AUTHENTICATIONS  = []string{"user", "admin", "subscriber"}
+)
+
+type User struct {
+	Username 	string `form:"username" json:"username" xml:"username" binding:"required"`
+	AuthType 	string `form:"authType" json:"authType" xml:"authType" binding:"required"`
+}
+
+func main() {
+
+	router := gin.Default()
+
+	store := sessions.NewCookieStore([]byte("sessionSuperSecret"))
+	router.Use(sessions.Sessions("sessionName", store))
+
+	api := router.Group("/api/v1")
+	// no authentication endpoints
+	{
+		api.POST("/login", loginHandler)
+		api.GET("/message/:msg", noAuthMessageHandler)
+	}
+	// basic authentication endpoints
+	{
+		basicAuth := api.Group("/")
+		basicAuth.Use(AuthenticationRequired("subscriber"))
+		{
+			basicAuth.GET("/logout", logoutHandler)
+			basicAuth.GET("/subscriber/message/:msg", subscriberMessageHandler)
+		}
+	}
+	// admin authentication endpoints
+	{
+		adminAuth := api.Group("/admin")
+		adminAuth.Use(AuthenticationRequired("admin"))
+		{
+			adminAuth.GET("/message/:msg", adminMessageHandler)
+		}
+	}
+
+	apiV2 := router.Group("/api/v2")
+	router.Use(AuthenticationRequired("admin", "subscriber"))
+	// admin and subscriber authentication endpoints
+	{
+		apiV2.POST("/post/message/:msg", postMessageHandler)
+	}
+
+	router.Run(":8080")
+}
+
+func loginHandler(c *gin.Context) {
+	var user User
+	if err := c.ShouldBind(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	session := sessions.Default(c)
+
+	if strings.Trim(user.Username, " ") == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "username can't be empty"})
+	}
+	if ! funk.ContainsString(VALID_AUTHENTICATIONS, user.AuthType) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid auth type"})
+	}
+
+	// Note: This is just an example, in real world AuthType would be set by business logic and not the user
+	session.Set("user", user.Username)
+	session.Set("authType", user.AuthType)
+
+	err := session.Save()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to generate session token"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "authentication successful"})
+}
+
+func logoutHandler(c *gin.Context) {
+	session := sessions.Default(c)
+
+	// this would only be hit if the user was authenticated
+	session.Delete("user")
+	session.Delete("authType")
+
+	err := session.Save()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to generate session token"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "successfully logged out"})
+
+}
+
+func noAuthMessageHandler(c *gin.Context) {
+	msg := c.Param("msg")
+	c.JSON(http.StatusOK, gin.H{"message": msg})
+}
+
+func subscriberMessageHandler(c *gin.Context) {
+	msg := c.Param("msg")
+
+	session := sessions.Default(c)
+	user := session.Get("user")
+
+	c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Hello Subscriber %s, here's your message: %s", user, msg)})
+}
+
+func adminMessageHandler(c *gin.Context) {
+	msg := c.Param("msg")
+
+	session := sessions.Default(c)
+	user := session.Get("user")
+
+	c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Hello Admin %s, here's your message: %s", user, msg)})
+}
+
+func postMessageHandler(c *gin.Context) {
+	msg := c.Param("msg")
+
+	session := sessions.Default(c)
+	user := session.Get("user")
+
+	c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Hello Admin/Subscriber %s, your message: %s will be posted", user, msg)})
+}
